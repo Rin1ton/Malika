dab746ccb7e234105350799349f740976b529e74
The_Shadow1098 <64919019+Rin1ton@users.noreply.github.com>
2021-12-06 23:45:41 -0500

    malika animates

--- Assets/Player/Scripts/PlayerAnimationBehavior.cs
+++ Assets/Player/Scripts/PlayerAnimationBehavior.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerAnimationBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public Animator myAnimator;
+	Rigidbody2D myRB;
+	float minSpeedToRun = 0.1f;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		myRB = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Mathf.Abs(myRB.velocity.x) >= minSpeedToRun)
+		{
+			if (myRB.velocity.x < 0)
+			{
+				myAnimator.SetBool("isRunningLeft", true);
+				myAnimator.SetBool("isRunningRight", false);
+			}
+			if (myRB.velocity.x > 0)
+			{
+				myAnimator.SetBool("isRunningLeft", false);
+				myAnimator.SetBool("isRunningRight", true);
+			}
+		}
+		else
+		{
+				myAnimator.SetBool("isRunningLeft", false);
+				myAnimator.SetBool("isRunningRight", false);
+		}
+	}
 }
